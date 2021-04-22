5584cc2b0bc490101840eee9117b36c98de38aa0
YOKI1216 <1151509140@qq.com>
2021-04-22 20:14:43 +0800

    Initial commit

--- src/Unuseful.java
+++ src/Unuseful.java
@@ -1,2 +1,6 @@
-package PACKAGE_NAME;public class Unuseful {
+public class Unuseful {
+    public static void main(String[] args) {
+        System.out.println("This is first step!");
+        System.out.println("???");
+    }
 }
